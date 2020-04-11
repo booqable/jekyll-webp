93cb9b2fb6997740b2f9b0a93852cff160720490
Johan van Zonneveld <johan@vzonneveld.nl>
2020-04-11 16:17:47 +0200

    🔧 Use hook

--- lib/jekyll-webp.rb
+++ lib/jekyll-webp.rb
@@ -3,7 +3,11 @@ require "jekyll-webp/defaults"
 require "jekyll-webp/webpExec"
 require "jekyll-webp/webpGenerator"
 
-module Jekyll 
+module Jekyll
   module Webp
   end # module Webp
 end # module Jekyll
+
+Jekyll::Hooks.register :site, :post_write, priority: :lowest do |site|
+  Jekyll::Webp::WebpGenerator.new.generate(site)
+end
